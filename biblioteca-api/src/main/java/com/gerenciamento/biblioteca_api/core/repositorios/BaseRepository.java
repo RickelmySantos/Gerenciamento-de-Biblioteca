c63661b4bd430578e493261b5e37c49b52a894cd
Rickelmy <rickrickelmy98@gmail.com>
2025-02-01 22:21:59 -0300

    feat: criação do repositorio generico

--- biblioteca-api/src/main/java/com/gerenciamento/biblioteca_api/core/repositorios/BaseRepository.java
+++ biblioteca-api/src/main/java/com/gerenciamento/biblioteca_api/core/repositorios/BaseRepository.java
@@ -1,5 +1,13 @@
 package com.gerenciamento.biblioteca_api.core.repositorios;
 
-public interface BaseRepository<E, Long> {
+import com.gerenciamento.biblioteca_api.core.modelo.EntidadeBase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(propagation = Propagation.SUPPORTS)
+public interface BaseRepository<E extends EntidadeBase>
+    extends JpaRepository<E, Long>, JpaSpecificationExecutor<E> {
 
 }
