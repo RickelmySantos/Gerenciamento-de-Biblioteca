c8ecfda550aeff2190c9e6cec0dff878f80f8418
Rickelmy <rickrickelmy98@gmail.com>
2025-03-15 22:52:45 -0300

    wip: estrutura inicial do component hero

--- biblioteca-ui/src/app/modules/biblioteca/layout/biblioteca-layout.component.ts
+++ biblioteca-ui/src/app/modules/biblioteca/layout/biblioteca-layout.component.ts
@@ -1,7 +1,11 @@
+import { AsyncPipe, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DataStore } from 'src/app/core/store/data.store';
 import { BaseComponent } from 'src/app/core/util/base.component';
+import { Livro } from 'src/app/models/livro.model';
 import { BibliotecaCategoriasViewComponent } from 'src/app/modules/biblioteca/layout/components/biblioteca-categoria/biblioteca-categorias-view.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CardRenderComponent } from 'src/app/shared/util/card/card-render.component';
 
 @Component({
     selector: 'app-biblioteca-layout',
@@ -10,6 +14,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
     standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush,
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    imports: [SharedModule, BibliotecaCategoriasViewComponent],
+    imports: [SharedModule, BibliotecaCategoriasViewComponent, CardRenderComponent, NgIf, AsyncPipe],
 })
-export class BibliotecaLayoutComponent extends BaseComponent {}
+export class BibliotecaLayoutComponent extends BaseComponent {
+    livroStore = new DataStore<Livro[]>();
+}
